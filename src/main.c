2a880c689080ae78e8f22e49a84beab5ac16c377
DuduCode03 <amandine.durando@gmail.com>
2024-10-28 11:25:03 +0100

    version code du cours avec config de la LED jaune

--- src/main.c
+++ src/main.c
@@ -1,14 +1,15 @@
-/*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+
 
 #include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
 
-int main(void)
-{
-	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+#define LED_YELLOW_NODE DT_ALIAS(led_yellow)
 
-	return 0;
+const struct gpio_dt_spec led_yellow_gpio = GPIO_DT_SPEC_GET_OR(LED_YELLOW_NODE, gpios, {0});
+
+
+int main(void) {
+	gpio_pin_configure_dt(&led_yellow_gpio, GPIO_OUTPUT_HIGH);
+
 }
