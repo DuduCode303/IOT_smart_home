1d5961fa3ed54a396208f77fe6fa9303d95be0f1
DuduCode03 <amandine.durando@gmail.com>
2024-10-28 11:54:57 +0100

    code modifié V1

--- src/main.c
+++ src/main.c
@@ -1,15 +1,15 @@
+#include <lcd_screen_i2c.h>
+#include <lcd_screen_i2c.c>
 
+//##############################################################################################
+// MAIN #########################################################################################
 
-#include <stdio.h>
-#include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
+int main() {
 
-#define LED_YELLOW_NODE DT_ALIAS(led_yellow)
+    // Init device
+    init_lcd(&dev_lcd_screen);
 
-const struct gpio_dt_spec led_yellow_gpio = GPIO_DT_SPEC_GET_OR(LED_YELLOW_NODE, gpios, {0});
-
-
-int main(void) {
-	gpio_pin_configure_dt(&led_yellow_gpio, GPIO_OUTPUT_HIGH);
-
+    // Display a message
+    write_lcd(&dev_lcd_screen, HELLO_MSG, LCD_LINE_1);
+    write_lcd_clear(&dev_lcd_screen, ZEPHYR_MSG LCD_LINE_2);
 }
