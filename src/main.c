d57a3b0daa951b7b0ab48b00f648f30a618eff59
DuduCode03 <amandine.durando@gmail.com>
2024-10-29 11:30:47 +0100

    maison fonctionnelle - affichage du HELLO_MSG et ZEHYR_MSG

--- src/main.c
+++ src/main.c
@@ -1,15 +1,32 @@
-#include <lcd_screen_i2c.h>
-#include <lcd_screen_i2c.c>
+#include "lcd_screen_i2c.h"
+#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/devicetree.h>
+
+
+#define LED_YELLOW_NODE DT_ALIAS(led_yellow)
+#define LCD_SCREEN_NODE DT_ALIAS(lcd_screen)
+
+static const struct i2c_dt_spec lcd_screen_dev = I2C_DT_SPEC_GET(LCD_SCREEN_NODE);
+const struct gpio_dt_spec led_yellow_gpio = GPIO_DT_SPEC_GET_OR(LED_YELLOW_NODE, gpios, {0});
+
 
 //##############################################################################################
 // MAIN #########################################################################################
 
 int main() {
 
+	// Init GPIO
+	gpio_pin_configure_dt(&led_yellow_gpio, GPIO_OUTPUT_HIGH);
+
     // Init device
-    init_lcd(&dev_lcd_screen);
+    init_lcd(&lcd_screen_dev);
 
     // Display a message
-    write_lcd(&dev_lcd_screen, HELLO_MSG, LCD_LINE_1);
-    write_lcd_clear(&dev_lcd_screen, ZEPHYR_MSG LCD_LINE_2);
+
+
+    write_lcd(&lcd_screen_dev, HELLO_MSG, LCD_LINE_1);
+    //write_lcd_clear(&dev_lcd_screen, ZEPHYR_MSG LCD_LINE_2);
 }
